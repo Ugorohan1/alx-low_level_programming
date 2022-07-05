b98b0933c5e36ee0168fbeb306eedd7f4ee40181
ugorohan1 <okarodaniel@gmail.com>
2022-07-05 12:32:51 +0000

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: it prints the word
+ * Return: always returns 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+
+}
