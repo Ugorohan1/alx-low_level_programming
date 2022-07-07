a153962fdd4b8416dfe6a38420e447df64d6c849
ugorohan1 <okarodaniel@gmail.com>
2022-07-07 13:16:29 +0000

    three is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,27 +1,10 @@
 #include "main.h"
 
 /**
- * _islower - Entry Point to function
- * @c: Tells the cases of  c
- *
- * Return: depends on the output
+ * _islower - checks is the character is lowercase
+ * Return: returns 1 and 0 depending on condition
  */
-
 int _islower(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (0);
-	}
-	else if (c >= 'a' && c <= 'z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-
-
-	return(0);
+	return (c >= 'a' && c <= 'z');
 }
