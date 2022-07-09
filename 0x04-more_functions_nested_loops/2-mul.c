a20825463c95c083cce6ee6b2b811d799307242e
ugorohan1 <okarodaniel@gmail.com>
2022-07-09 22:57:01 +0000

    multiply this

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - a program that mutiplies two integers
+ *
+ * Return: return result
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 int _isdigit(int c);
 int _putchar(char c);
 int _isupper(int c);
+int mul(int a, int b);
 
 #endif
