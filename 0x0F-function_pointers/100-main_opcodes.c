8556622e5864b11299e49afaee73102a1ae82825
ugorohan1 <okarodaniel@gmail.com>
2022-08-05 03:21:33 +0000

    As long as you remain adaptable

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -20,6 +20,13 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(1);
 	}
+	number_of_bytes = atoi(argv[1]);
+
+	if (number_of_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
 	i = 0;
 	while (i < number_of_bytes)
