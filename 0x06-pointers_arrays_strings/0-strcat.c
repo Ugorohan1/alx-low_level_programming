420a119c9143687acb9758fe0760da8d18f7c433
ugorohan1 <okarodaniel@gmail.com>
2022-09-23 16:00:06 +0000

    made the string voncatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - a function that concatenates two strings
+ * @src: source string
+ * @dest: destination string
+ *
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, len;
+
+	len = strlen(dest);
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
+}
