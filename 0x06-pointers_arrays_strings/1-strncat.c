c36ebe04b3d7f5007bc99ee8911d7bf138851da0
ugorohan1 <okarodaniel@gmail.com>
2022-09-26 20:31:51 +0000

    concatenate and dont forget the null byte

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,7 @@
 
 /**
  * _strncat -> a function to contenate two strigs
- * @dest: first string 
+ * @dest: first string
  * @src: second string
  * @n: the number of bytes
  *
@@ -12,16 +12,15 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	char result[1000];
-	int i = 0, l = 0;
-	
+	int i = 0;
+	int l = 0;
+
 	while (dest[i] != '\0')
 	{
 		result[l] = dest[i];
-		l++;
 		i++;
+		l++;
 	}
-	result[l] = '\0';
-	
 	for (i = 0; src[i] && i < n; i++)
 	{
 		result[l] = src[i];
@@ -33,10 +32,10 @@ char *_strncat(char *dest, char *src, int n)
 	while (result[l] != '\0')
 	{
 		dest[i] = result[l];
-		i++;
 		l++;
+		i++;
 	}
 	dest[i] = '\0';
 
-	return(dest);
+	return (dest);
 }
