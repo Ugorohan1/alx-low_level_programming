036930e7304da3efad928523f9bc103acd35779f
ugorohan1 <okarodaniel@gmail.com>
2022-09-30 13:47:59 +0000

    it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main - a program that prints 98
+ * main - a program that prints 
  *
  * Return: return always 0.
  */
+
 int main(void)
 {
 	int n;
@@ -11,8 +12,17 @@ int main(void)
 	int *p;
 
 	a[2] = 1024;
-	p = &n;
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
 	*(a + 2) = 98;
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
+
