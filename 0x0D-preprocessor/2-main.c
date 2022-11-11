7f6695074425781ea76b9277720c560df2e49aeb
ugorohan1 <okarodaniel@gmail.com>
2022-11-11 15:24:49 +0000

    a function-like macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: Program that prints the name of the file
+ *
+ * Return: Success.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
